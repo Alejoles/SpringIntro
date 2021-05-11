d54a9b52c279dd0b7368e78f289060d84890ce84
Alejo <rulitosxd1@gmail.com>
2021-05-10 20:24:28 -0500

    Some stuff of security

---
+++ demo1/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .authorizeRequests()
+          // Whitelist some pages to everyone in those pages isn't necessary to log in
+          //.antMatchers("/", "index", "/css/*", "/js/*")
+          //.permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic();
+    }
+
+}
